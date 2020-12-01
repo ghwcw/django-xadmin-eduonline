629f28ac6d757b4dc57aaa50f5b253656a976632
wong.cw <39757711+ghwcw@users.noreply.github.com>
2020-12-01 13:11:30 +0800

    Update settings.py

--- eduonline/settings.py
+++ eduonline/settings.py
@@ -132,10 +132,10 @@ USE_L10N = True               # 本地化
 USE_TZ = False                # 若使用了本地时间，必须设为False!!(默认值True)
 
 # 邮件服务配置
-EMAIL_HOST = 'smtp.exmail.qq.com'  # 发送者邮箱服务器
+EMAIL_HOST = 'smtp.qq.com'  # 发送者邮箱服务器
 EMAIL_PORT = 25
-EMAIL_HOST_USER = 'wangchunwang@dhcc.com.cn'  # 发送者用户名（邮箱地址）
-EMAIL_HOST_PASSWORD = 'DHC@wcw070134'  # 发送者密码
+EMAIL_HOST_USER = 'wang@qq.com'  # 发送者用户名（邮箱地址）
+EMAIL_HOST_PASSWORD = '123456'  # 发送者密码
 EMAIL_USE_SSL = False
 
 # 静态文件配置 (CSS, JavaScript, Images)
